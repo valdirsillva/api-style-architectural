3204250d7aefd7e9c9a0628a73235e7820acac43
Valdir Silva <valdipiresba@gmail.com>
2023-08-30 21:52:02 -0300

    Implementa rota get para listar pessoas

--- api-rest-contatos/src/api-v1/paths/router-person.js
+++ api-rest-contatos/src/api-v1/paths/router-person.js
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import personModel from '../models/person-model';
 
 const personRouter = Router();
 
-personRouter.use('/', function (req, res, next) {
-    res.send('Endpoint pessoa')
-})
+// personRouter.use('/', function (req, res, next) {
+//     res.send('Endpoint pessoa')
+// })
+
+personRouter.get('/', listPersons);
+
+function listPersons(req, res, next) {
+    personModel.list({}, (err, list) => {
+        if (!err) {
+            res.json(list)
+        } else {
+            res.status(400).send(err.message);
+        }
+    })
+}
 
 export { personRouter }
